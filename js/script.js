89059d678e766e34b5b66129839c23ce9e32e309
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-02-09 03:55:38 +0200

    homework

--- js/script.js
+++ js/script.js
@@ -7,15 +7,13 @@ const priceData = {
   BANANAS: "48",
   oRAnGEs: "48.7584",
 };
-console.log(priceData);
 
-let keys = Object.keys(priceData).join().toLowerCase().split(",");
-console.log(keys);
-
-let values = Object.values(priceData);
-for (let value of values) {
-  console.log(parseFloat(value).toFixed(2).split(","));
+for (let [keys, values] of Object.entries(priceData)) {
+  let items = keys.toLowerCase().split();
+  let prices = Number(values);
+  let newArray = items.concat(prices.toFixed(2).split());
+  console.log(newArray);
 }
 
-let updatedPriceData = Object.fromEntries(priceData);
+let updatedPriceData = Object.fromEntries(newArray);
 console.log(updatedPriceData);
