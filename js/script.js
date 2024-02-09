921b1e6a7fe104435deee3b2347c2a260d8f66a9
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-02-09 04:21:40 +0200

    homework

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ const priceData = {
   oRAnGEs: "48.7584",
 };
 
+let updatedPriceData = {};
+
 for (let [keys, values] of Object.entries(priceData)) {
-  let items = keys.toLowerCase().split();
+  let items = keys.toLowerCase();
   let prices = Number(values);
-  let newArray = items.concat(prices.toFixed(2).split());
-  console.log(newArray);
+  let roundedPrices = prices.toFixed(2);
+  updatedPriceData[items] = roundedPrices;
 }
 
-let updatedPriceData = Object.fromEntries(newArray);
 console.log(updatedPriceData);
